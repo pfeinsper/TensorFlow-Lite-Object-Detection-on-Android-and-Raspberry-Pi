54c7e2ed5687cedd779fae4d40c1bc3dc35a7a36
fontes99 <pvfontes1000@gmail.com>
2021-11-09 14:57:37 -0300

    bug fixing

--- install_object_detection_API.sh
+++ install_object_detection_API.sh
@@ -1,12 +1,22 @@
 pip3 uninstall tensorflow -y
-pip3 uninstall keras -y 
-
 pip3 install tensorflow==2.6.0
+
+pip3 uninstall keras -y 
 pip3 install keras==2.6.0
+
 
 git clone https://github.com/tensorflow/models.git
 
 cd models/research
+
+git clone https://github.com/cocodataset/cocoapi.git
+
+cd cocoapi/PythonAPI
+python setup.py build_ext --inplace
+python setup.py build_ext install
+cd ../..
+
+cp -r pycocotools .
 
 # Compile protos.
 sudo apt install protobuf-compiler -y
@@ -16,4 +26,4 @@ protoc object_detection/protos/*.proto --python_out=.
 cp object_detection/packages/tf2/setup.py .
 python3 -m pip install --use-feature=2020-resolver .
 
-
+python3 object_detection/builders/model_builder_tf2_test.py
