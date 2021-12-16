cec42c57d37eeae94dcc367a0d4e27c59d650253
slimkaki <rafaalmada98@gmail.com>
2021-12-16 18:58:45 -0300

    minor fix on query mode

--- play_voice.py
+++ play_voice.py
@@ -9,14 +9,14 @@ from gtts import gTTS
 from pydub import AudioSegment
 from pydub.playback import play
 
-def play_voice(mText, lang="en"):
-    """Function used to play the string 'mText' in audio using tts"""
-    print(f"[play_voice] now playing: '{mText}'")
-    tts_audio = gTTS(text=mText, lang=lang, slow=False)
+# def play_voice(mText, lang="en"):
+#     """Function used to play the string 'mText' in audio using tts"""
+#     print(f"[play_voice] now playing: '{mText}'")
+#     tts_audio = gTTS(text=mText, lang=lang, slow=False)
 
-    tts_audio.save("audio_recognition/voice.wav")
-    play(AudioSegment.from_file("audio_recognition/voice.wav"))
-    os.remove("audio_recognition/voice.wav")
+#     tts_audio.save("audio_recognition/voice.wav")
+#     play(AudioSegment.from_file("audio_recognition/voice.wav"))
+#     os.remove("audio_recognition/voice.wav")
 
 # def play_voice(mText, lang='en'):
     # Thread(target=play_v, args=(mText, lang)).start()
